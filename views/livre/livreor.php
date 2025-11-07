1ace8e6cba7519898008a98231e10eba154d01bf
nina-penot <nina.penot@laplateforme.io>
2025-11-07 14:34:53 +0100

    Ajout de décoration pour les message
    -Les noms d'auteurs sont maintenant décorés avec des splashs de peinture au hasard avec des couleurs choisies au hasard

--- views/livre/livreor.php
+++ views/livre/livreor.php
@@ -3,11 +3,29 @@
 <div>Vous souhaitez aussi laisser un message ?</div>
 <?php generate_neon_link("Postez un message.", 'livre/commentaire') ?>
 
+<!-- <div style="height: 120px; object-fit: contain;" class="pos_relative">
+    <img style="height: inherit;" class="svg_color_blue" src="../../public/assets/images/blob_3.svg" alt="splosh">
+    <img style="height: inherit;" class="svg_color_pink pos_absolute paint_coords" src="../../public/assets/images/blob_3.svg" alt="splosh">
+    <div style="gap: 10px;" class="pos_absolute paint_txt_coords float_left">
+        <div>👤</div>
+        <div>TEXTZZZZZZZZZZZZZZZZ</div>
+    </div>
+</div> -->
+
 <div class="message_all">
 
     <?php
+    $colors = ['pink', 'blue', 'yellow'];
     $my_comments = get_limited_comments($per_page, $page);
     foreach ($my_comments as $num => $c) {
+        $num1 = mt_rand(0, count($colors) - 1);
+        $num2 = mt_rand(0, count($colors) - 1);
+        $blob_num = mt_rand(1, 3);
+        while ($num2 == $num1) {
+            $num2 = mt_rand(0, count($colors) - 1);
+        }
+        $rand_color1 = $colors[$num1];
+        $rand_color2 = $colors[$num2];
         if ($num % 2 == 0) {
             $direction1 = "go_right";
             $direction2 = "go_left";
@@ -16,17 +34,23 @@
             $direction1 = "go_left";
         } ?>
         <div class="message_main <?= $direction1 ?>">
-            <div class="message_profile <?= $direction2 ?> float_left">
-                <div class="message_profile_img">👤</div>
-                <div class="message_profile_txt"><?= escape(get_comment_author($c["id"])) ?></div>
+            <div style="height: 120px; top: -20px" class="pos_absolute <?= $direction2 ?>">
+                <img style="height: inherit;" class="svg_color_<?= $rand_color1 ?>" src="../../public/assets/images/blob_<?= $blob_num ?>.svg" alt="splosh">
+                <img style="height: inherit;" class="svg_color_<?= $rand_color2 ?> pos_absolute paint_coords" src="../../public/assets/images/blob_<?= $blob_num ?>.svg" alt="splosh">
+                <div style="gap: 10px;" class="pos_absolute paint_txt_coords float_left">
+                    <div>👤</div>
+                    <div class="paint_text_style"> <strong> <?= escape(get_comment_author($c["id"])) ?> </strong></div>
+                </div>
             </div>
 
             <div class="message_text_block">
                 <div class="message_text">
-                    <?= escape($c['commentaire']) ?>
-                </div>
-                <div class="message_text">
-                    <?= escape($c['date']) ?>
+                    <div>
+                        <?= escape($c['commentaire']) ?>
+                    </div>
+                    <p>
+                        <?= escape($c['date']) ?>
+                    </p>
                 </div>
             </div>
 
